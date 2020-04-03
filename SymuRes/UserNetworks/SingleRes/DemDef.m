61ee8fdfe499721b02ddc739b88847478b72ab1d
Guilhem Mariotte <61288546+guilhemmariotte@users.noreply.github.com>
2020-04-03 02:55:42 +0200

    Add files via upload

--- SymuRes/UserNetworks/SingleRes/DemDef.m
+++ SymuRes/UserNetworks/SingleRes/DemDef.m
@@ -10,155 +10,107 @@ for od = 1:NumODmacro
     ODmacro(od).Demand(i).Data = 0; % [veh/s]
 end
 
+addpath(['UserNetworks/' Simulation.Network '/scenarios/'])
 
-%% Scenario 1: predefined routes
+
+%% Scenario 1: one trip, demand peak
 %--------------------------------------------------------------------------
+% Fig. 4.a1.b1.c1 (section 2.3) of Mariotte & Leclercq (Part B 2019)
 
-% Assignment.Periods = [0 Simulation.Duration];
-% Assignment.PredefRoute = 1;
-% Assignment.Convergence = 0;
-% 
-% % [R1 R2 R4]
-% %--------------------------------------------------------------------------
-% iroute = 1;
-% for od = 1:NumODmacro
-%     if ODmacro(od).NodeOriginID == 1 && ODmacro(od).NodeDestinationID == 3
-%         od0 = od;
-%     end
-% end
-% Route(iroute).ODmacroID = od0;
-% Route(iroute).ResPath = ODmacro(od0).PossibleRoute(1).ResPath;
-% Route(iroute).NodePath = ODmacro(od0).PossibleRoute(1).NodePath;
-% Route(iroute).TripLengths = ODmacro(od0).PossibleRoute(1).TripLengths;
-% j = 1;
-% Route(iroute).Demand0(j).Purpose = 'cartrip';
-% Td1 = 500;
-% Td11 = 1000;
-% Td12 = 2000;
-% Td2 = 2500;
-% q0 = 0.35;
-% q1 = 1.1;
-% q2 = 0.35;
-% % qinD = @(t_) q0*bumpfct(t_,Td1,Td2,q1/q0,q2/q0);
-% qinD = @(t_) q0*trayfct(t_,Td1,Td11,Td12,Td2,q1/q0,q2/q0);
-% Route(iroute).Demand0(j).Time = [0 Td1:60:(Td2+60)]; % [s]
-% Route(iroute).Demand0(j).Data = [q0 qinD(Td1:60:Td2) q2]; % [veh/s]
-% 
-% % [R1 R2 R3 R4]
-% %--------------------------------------------------------------------------
-% iroute = 2;
-% for od = 1:NumODmacro
-%     if ODmacro(od).NodeOriginID == 1 && ODmacro(od).NodeDestinationID == 3
-%         od0 = od;
-%     end
-% end
-% Route(iroute).ODmacroID = od0;
-% Route(iroute).ResPath = ODmacro(od0).PossibleRoute(3).ResPath;
-% Route(iroute).NodePath = ODmacro(od0).PossibleRoute(3).NodePath;
-% Route(iroute).TripLengths = ODmacro(od0).PossibleRoute(3).TripLengths;
-% j = 1;
-% Route(iroute).Demand0(j).Purpose = 'cartrip';
-% Td1 = 500;
-% Td11 = 1000;
-% Td12 = 1500;
-% Td2 = 2000;
-% q0 = 0.2;
-% q1 = 0.9; %1.2;
-% q2 = 0.2;
-% % qinD = @(t_) q0*bumpfct(t_,Td1,Td2,q1/q0,q2/q0);
-% qinD = @(t_) q0*trayfct(t_,Td1,Td11,Td12,Td2,q1/q0,q2/q0);
-% Route(iroute).Demand0(j).Time = [0 Td1:60:(Td2+60)]; % [s]
-% Route(iroute).Demand0(j).Data = [q0 qinD(Td1:60:Td2) q2]; % [veh/s]
-% 
-% % [R1 R3 R4]
-% %--------------------------------------------------------------------------
-% iroute = 3;
-% for od = 1:NumODmacro
-%     if ODmacro(od).NodeOriginID == 1 && ODmacro(od).NodeDestinationID == 3
-%         od0 = od;
-%     end
-% end
-% Route(iroute).ODmacroID = od0;
-% Route(iroute).ResPath = ODmacro(od0).PossibleRoute(2).ResPath;
-% Route(iroute).NodePath = ODmacro(od0).PossibleRoute(2).NodePath;
-% Route(iroute).TripLengths = ODmacro(od0).PossibleRoute(2).TripLengths;
-% j = 1;
-% Route(iroute).Demand0(j).Purpose = 'cartrip';
-% Td1 = 500;
-% Td11 = 1000;
-% Td12 = 2000;
-% Td2 = 2500;
-% q0 = 0.1;
-% q1 = 0.2;
-% q2 = 0.1;
-% % qinD = @(t_) q0*bumpfct(t_,Td1,Td2,q1/q0,q2/q0);
-% qinD = @(t_) q0*trayfct(t_,Td1,Td11,Td12,Td2,q1/q0,q2/q0);
-% Route(iroute).Demand0(j).Time = [0 Td1:60:(Td2+60)]; % [s]
-% Route(iroute).Demand0(j).Data = [q0 qinD(Td1:60:Td2) q2]; % [veh/s]
-% 
-% MacroNode(9).Capacity.Time = [0 1000];
-% MacroNode(9).Capacity.Data = [10 0.4];
-
-
-%% Scenario 2: one assignment period
-%--------------------------------------------------------------------------
-
-% Assignment.Periods = [0 Simulation.Duration];
-% Assignment.Convergence = 1;
-% Assignment.PredefRoute = 0;
-% Assignment.model = 1; % DUE
-% Assignment.Behavior = 1; % rational
-% 
-% % R1 > R4
-% %--------------------------------------------------------------------------
-% for od = 1:NumODmacro
-%     if ODmacro(od).NodeOriginID == 1 && ODmacro(od).NodeDestinationID == 3
-%         od0 = od;
-%     end
-% end
-% i = 1;
-% ODmacro(od0).Demand(i).Purpose = 'cartrip';
-% Td1 = 1000;
-% Td11 = 1500;
-% Td12 = 2500;
-% Td2 = 3000;
-% q0 = 0.5;
-% q1 = 1.9; % 1.3;
-% q2 = 0.5;
-% % qinD = @(t_) q0*bumpfct(t_,Td1,Td2,q1/q0,q2/q0);
-% qinD = @(t_) q0*trayfct(t_,Td1,Td11,Td12,Td2,q1/q0,q2/q0);
-% ODmacro(od0).Demand(i).Time = [0 Td1:60:(Td2+60)]; % [s]
-% ODmacro(od0).Demand(i).Data = [q0 qinD(Td1:60:Td2) q2]; % [veh/s]
-
-
-%% Scenario 3: several assignment periods
-%--------------------------------------------------------------------------
-
-Assignment.Periods = [0 1000 2000 3000 4000 Simulation.Duration];
-Assignment.Convergence = 1;
-Assignment.PredefRoute = 0;
-Assignment.model = 1; % DUE
-Assignment.Behavior = 1; % rational
-
-% R1 > R4
-%--------------------------------------------------------------------------
-for od = 1:NumODmacro
-    if ODmacro(od).NodeOriginID == 1 && ODmacro(od).NodeDestinationID == 3
-        od0 = od;
-    end
+if strcmp(Simulation.Name,'SC11')
+    Simulation.DivergeModel = 'maxdem'; % 'maxdem', 'decrdem' or 'queuedyn'
+    DemSC1
 end
-i = 1;
-ODmacro(od0).Demand(i).Purpose = 'cartrip';
-Td1 = 1000;
-Td11 = 1500;
-Td12 = 2500;
-Td2 = 3000;
-q0 = 0.5;
-q1 = 1.9; % 1.3;
-q2 = 0.5;
-% qinD = @(t_) q0*bumpfct(t_,Td1,Td2,q1/q0,q2/q0);
-qinD = @(t_) q0*trayfct(t_,Td1,Td11,Td12,Td2,q1/q0,q2/q0);
-ODmacro(od0).Demand(i).Time = [0 Td1:60:(Td2+60)]; % [s]
-ODmacro(od0).Demand(i).Data = [q0 qinD(Td1:60:Td2) q2]; % [veh/s]
 
+if strcmp(Simulation.Name,'SC12')
+    Simulation.DivergeModel = 'decrdem'; % 'maxdem', 'decrdem' or 'queuedyn'
+    DemSC1
+end
+
+
+%% Scenario 2: one trip, supply reduction
+%--------------------------------------------------------------------------
+% Fig. 4.a2.b2.c2 (section 2.3) of Mariotte & Leclercq (Part B 2019)
+
+if strcmp(Simulation.Name,'SC21')
+    Simulation.DivergeModel = 'maxdem'; % 'maxdem', 'decrdem' or 'queuedyn'
+    DemSC2
+end
+
+if strcmp(Simulation.Name,'SC22')
+    Simulation.DivergeModel = 'decrdem'; % 'maxdem', 'decrdem' or 'queuedyn'
+    DemSC2
+end
+
+
+%% Scenario 3: two trips, demand surge
+%--------------------------------------------------------------------------
+% Fig. 8 (section 4.1) of Mariotte & Leclercq (Part B 2019)
+
+if strcmp(Simulation.Name,'SC31')
+    Simulation.MergeModel = 'demprorata'; % 'demprorata', 'demfifo', 'equiproba' or 'endogenous'
+    Simulation.DivergeModel = 'decrdem'; % 'maxdem', 'decrdem' or 'queuedyn'
+    DemSC3
+end
+
+if strcmp(Simulation.Name,'SC32')
+    Simulation.MergeModel = 'demprorata'; % 'demprorata', 'demfifo', 'equiproba' or 'endogenous'
+    Simulation.DivergeModel = 'maxdem'; % 'maxdem', 'decrdem' or 'queuedyn'
+    DemSC3
+end
+
+if strcmp(Simulation.Name,'SC33')
+    Simulation.MergeModel = 'endogenous'; % 'demprorata', 'demfifo', 'equiproba' or 'endogenous'
+    Simulation.DivergeModel = 'maxdem'; % 'maxdem', 'decrdem' or 'queuedyn'
+    DemSC3
+end
+
+
+%% Scenario 4: two trips, demand peak
+%--------------------------------------------------------------------------
+% Not presented in the paper
+
+if strcmp(Simulation.Name,'SC41')
+    Simulation.MergeModel = 'demprorata'; % 'demprorata', 'demfifo', 'equiproba' or 'endogenous'
+    Simulation.DivergeModel = 'decrdem'; % 'maxdem', 'decrdem' or 'queuedyn'
+    DemSC4
+end
+
+if strcmp(Simulation.Name,'SC42')
+    Simulation.MergeModel = 'demprorata'; % 'demprorata', 'demfifo', 'equiproba' or 'endogenous'
+    Simulation.DivergeModel = 'maxdem'; % 'maxdem', 'decrdem' or 'queuedyn'
+    DemSC4
+end
+
+if strcmp(Simulation.Name,'SC43')
+    Simulation.MergeModel = 'endogenous'; % 'demprorata', 'demfifo', 'equiproba' or 'endogenous'
+    Simulation.DivergeModel = 'maxdem'; % 'maxdem', 'decrdem' or 'queuedyn'
+    DemSC4
+end
+
+
+%% Scenario 5: two trips, supply reduction
+%--------------------------------------------------------------------------
+% Fig. 9 (section 4.2) of Mariotte & Leclercq (Part B 2019)
+
+if strcmp(Simulation.Name,'SC51')
+    Simulation.MergeModel = 'demprorata'; % 'demprorata', 'demfifo', 'equiproba' or 'endogenous'
+    Simulation.DivergeModel = 'decrdem'; % 'maxdem', 'decrdem' or 'queuedyn'
+    DemSC5
+end
+
+if strcmp(Simulation.Name,'SC52')
+    Simulation.MergeModel = 'demprorata'; % 'demprorata', 'demfifo', 'equiproba' or 'endogenous'
+    Simulation.DivergeModel = 'maxdem'; % 'maxdem', 'decrdem' or 'queuedyn'
+    DemSC5
+end
+
+if strcmp(Simulation.Name,'SC53')
+    Simulation.MergeModel = 'endogenous'; % 'demprorata', 'demfifo', 'equiproba' or 'endogenous'
+    Simulation.DivergeModel = 'maxdem'; % 'maxdem', 'decrdem' or 'queuedyn'
+    DemSC5
+end
+
+
+rmpath(['UserNetworks/' Simulation.Network '/scenarios/'])
 
